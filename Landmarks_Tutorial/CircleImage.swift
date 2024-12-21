45b116c54d471916f2c90825e416aacf78140778
89Explorer <leral3@naver.com>
2024-12-22 08:02:41 +0900

    Feat: 새로운 커스텀뷰 생성
    - 이미지뷰 생성
    - 이미지뷰에 속성 설정
    - 여기서 shadow, overlay간의 호출 순서 중요함 (호출 순서에 따라 효과가 다름)

---
+++ Landmarks_Tutorial/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks_Tutorial
+//
+//  Created by 권정근 on 12/22/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+        // overlay와 shadow 간의 호출 순서는 중요함
+        // 호출 순서에 따라 효과가 다름
+    }
+}
+
+#Preview {
+    CircleImage()
+}
