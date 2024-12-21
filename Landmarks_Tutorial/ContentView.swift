96cdd675846042999f9283cf89a15554c850dc88
89Explorer <leral3@naver.com>
2024-12-22 07:32:33 +0900

    feat: 실시간으로 변경하는 방법
    - 캔버스에서 선택모드 누르면, 캔버스에서 텍스트 선택 가능
    - 캔버스에서 Command - Control - Click 하면 "Show SwiftUI Inspector"를 통해 프로퍼티 설정 가능
    - 코드에서는 Command - Click 으로 가능

--- Landmarks_Tutorial/ContentView.swift
+++ Landmarks_Tutorial/ContentView.swift
@@ -12,7 +12,23 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            VStack {
+                Text("Turtle Rock")
+                    .font(.largeTitle)
+                    
+                Text("Hello, Man!")
+                    .fontWeight(.black)
+                    .foregroundColor(Color.red)
+                
+            }
             Text("Hello, Man!")
+            Text("Hello, Man!")
+            Text("Hello, Man!")
+            HStack {
+                Text("Hello, Man!")
+                Text("Hello, Man!")
+                Text("Hello, Man!")
+            }
         }
         .padding()
     }
